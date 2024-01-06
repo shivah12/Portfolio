ae9b720a21910b14ce76f90678d9062216d3b2f7
Shreeti <114981873+shivah12@users.noreply.github.com>
2024-01-06 12:52:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+// Header Scroll
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+} 
+
+// nav hide 
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+ 
+// Add animate__fadeIn class to trigger the fade-in effect
+document.querySelector('.banner-content').classList.add('animate__fadeIn');
